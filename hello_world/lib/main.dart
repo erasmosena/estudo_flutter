d233f8932a4ce3e892cad5a0974bf161b900c43f
Erasmo Sena <erasmo.sena@gmail.com>
2019-12-14 11:36:51 -0300

    app completo com função de alterar tamanho da fonte

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -4,21 +4,108 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class RandomWordsState extends State<RandomWords> {
-  @override                                  
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  TextStyle _biggerFont = const TextStyle(fontSize: 18);
+  void _incFont() {
+    setState(() {
+      _biggerFont = const TextStyle(fontSize: 30);  
+    });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+
+          final List<Widget> divided =
+              ListTile.divideTiles(context: context, tiles: tiles).toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Sugestion'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    final WordPair wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          ),
+          IconButton(
+            icon: Icon(Icons.more ) ,
+            onPressed: _incFont,
+          )
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 }
 
 class RandomWords extends StatefulWidget {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18); 
-  
   @override
   RandomWordsState createState() => RandomWordsState();
-
-  
 }
 
 class MyApp extends StatelessWidget {
@@ -26,15 +113,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          //child: const Text('Hello World'),
-          child: RandomWords(),  // With this text.
-        ),
+      theme: ThemeData(
+        primaryColor: Colors.white,
       ),
+      home: RandomWords(),
     );
   }
 }
