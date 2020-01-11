3a1c544fcd13fec15d2d90d45c7372e82a332d47
Erasmo Sena <erasmo.sena@gmail.com>
2020-01-11 15:39:01 -0300

    ignore config file google

---
+++ udemy/Ciolfi/chat/lib/ui/chat_message.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage(this.data,this.mine);
+  
+  final Map<String,dynamic> data ;
+  final bool mine;
+
+
+  @override
+  Widget build(BuildContext context) {
+    Widget msg = data["imgUrl"] != null ?
+      Image.network(data["imgUrl"],width: 250,): 
+      Text(
+        data["text"],
+        style: TextStyle(
+          fontSize: 20
+        ),
+      );
+    return Container(
+      margin:const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: 
+          mine?
+         _getMyRowMsg(msg):
+         _getOthersRowMsg(msg)
+    );
+  }
+
+  Widget _getMyRowMsg(Widget msg){
+    return Row(
+      children: <Widget>[
+        
+        Expanded(
+          child: Column(
+
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                data["senderName"],
+                style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500
+                ),
+              ),msg,
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(data["senderPhotoUrl"]),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _getOthersRowMsg(Widget msg){
+    
+    return Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(data["senderPhotoUrl"]),
+            ),
+          ),
+          Expanded(
+            child: Column(
+
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                msg,
+                Text(
+                  data["senderName"],
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      );
+  }
+}
