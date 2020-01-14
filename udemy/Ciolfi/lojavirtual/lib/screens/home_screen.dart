3bb98ce0543f48adbd54d415b3432023b66fe40f
Erasmo Sena <erasmo.sena@gmail.com>
2020-01-14 14:02:10 -0300

    menu categorias

--- udemy/Ciolfi/lojavirtual/lib/screens/home_screen.dart
+++ udemy/Ciolfi/lojavirtual/lib/screens/home_screen.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/tabs/home_tab.dart';
+import 'package:lojavirtual/tabs/category_tab.dart';
 import 'package:lojavirtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
-  final PageController pageController = PageController();
+  final PageController _pageController = PageController();
   
   @override
   Widget build(BuildContext context) {
     return PageView(
-      controller: pageController,
+      controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
         Scaffold(
           body: HomeTab(),
-          drawer: CustomDrawer(pageController),
+          drawer: CustomDrawer(_pageController),
         ),
-
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          body: CategoryTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
       ],
     );
   }
